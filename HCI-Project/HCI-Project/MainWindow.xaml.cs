6b03f3703ae9b933d48f84f7b895abead564cc80
Aksont <aastanivuk@gmail.com>
2022-06-14 06:11:37 +0200

    merge fix

--- HCI-Project/HCI-Project/MainWindow.xaml.cs
+++ HCI-Project/HCI-Project/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace HCI_Project
         {
             new UserRepo();
             InitializeComponent();
-            InitializeData();
+            InitializeData(); 
             DataContext = this;
         }
 
